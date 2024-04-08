7846fe77ee5f01d573b09a7b1bdbd66d3fea1254
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-04-08 11:26:02 +0300

    dead

--- controllers/edit.php
+++ controllers/edit.php
@@ -1,18 +1,20 @@
 <?php
 require "Database.php";
 $config = require "config.php";
+require "Validator.php";
+$db = new Database($config);
 
 
-if(isset($_GET["datetime"]) && isset($_GET["event"]) && isset($_GET["place"]))
+if(!Validator::string($_POST["datetime"], min: 1, max:500) && isset($_GET["event"]) && isset($_GET["place"]))
 {
     $query = "UPDATE ckc_events
     SET datetime = :datetime, event = :event, place = :place
     WHERE id = :id;";
     $params = [
-    ':datetime' => $_GET['datetime'], ///chats palidzeja lol
-    ':event' => $_GET['event'],
-    ':place' => $_GET['place'],
-    ':id' => $_GET['id']
+    ':datetime' => $_POST['datetime'], ///chats palidzeja lol
+    ':event' => $_POST['event'],
+    ':place' => $_POST['place'],
+    ':id' => $_POST['id']
     ];
     $db = new Database($config);
     $events = $db->execute($query, $params)->fetchALL(); 
@@ -35,4 +37,48 @@ else
 
 
 require "views/edit.view.php";
+
+<?php 
+require "Validator.php";
+require "Database.php";
+$config = require("config.php");
+$db = new Database($config);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $errors = [];
+
+    if (!Validator::string($_POST["event"], min: 1, max: 255)) {
+        $errors["title"] = "title cannot be empty or too long";
+    }if{
+        !Validator::string($_POST["place"], min: 1, max: 255)) {
+            $errors["title"] = "title cannot be empty or too long";
+    }
+
+    if (!Validator::number($_POST["datetime"], min:1,max:3)){
+        $errors["category-id"] = "category id invalid";
+    }
+    if (empty($errors)) {
+        $query = "UPDATE posts
+        SET title = :title, category_id = :category_id
+        WHERE id = :id";
+        $params = [
+        ":title" => $_POST["title"],
+        ":category_id" => $_POST["category-id"],
+        ":id" => $_POST["id"]
+    ];
+   
+        $db->execute($query, $params);
+        header('location: /');
+        die();
+
+ }
+}
+$query = "SELECT * FROM posts WHERE id = :id";
+$params = [":id" => $_GET["id"]];
+
+$post = $db->execute($query, $params)->fetch();
+
+
+require "views/posts/edit.view.php";
 
---
+++ routes.php
@@ -0,0 +1,13 @@
+<?php
+return[
+    "/" => "controllers/index.php",
+    "/kolektivi" => "controllers/kolektivi.php",
+    "/add" => "controllers/add.php",
+    "/delete" => "controllers/delete.php",
+    "/edit" => "controllers/edit.php",
+    "/add-kolektivi" => "controllers/add-kolektivi.php",
+    "/login" => "controllers/login.php",
+    "/deleteKolektiv" => "controllers/kolektivi-delete.php",
+    "/show" => "controllers/show.php",
+    "/shows" => "controllers/show-kolektivi.php"
+];
